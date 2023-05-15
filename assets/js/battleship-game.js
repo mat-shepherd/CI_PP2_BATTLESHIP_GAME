9a87a35f63e74f9e0a795beb16a6935d75fa3631
mat-shepherd <>
2023-05-15 15:09:41 +0000

    Feat: add loop to gameBoard() to print player grids and test placement of ship and effects

--- assets/js/battleship-game.js
+++ assets/js/battleship-game.js
@@ -4,6 +4,33 @@ function gameBoard() {
     let computerGameboard = document.getElementById('computer-gameboard');
     let gridLetters = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
     let playGrid = '';
+    for (let i = 0; i <= 10; i++) {
+        if (i == 0) {
+            for (let j = 0; j <= 10; j++) {
+                playGrid += `<div id='iR${j}' class="indexRow">${j == 0 ? ' ' : j}</div>`;
+            }
+        } else {
+            for (let j = 0; j <= 10; j++) {
+                if (j == 0) {
+                    playGrid += `<div id='iC ${gridLetters[j + i].trim()}' class="indexColumn">${gridLetters[j + i].trim()}</div>`;
+                } else {
+                    playGrid += `<div id='p${j + (i - 1) * 10}' class="playableArea">${j + (i - 1) * 10}</div>`;
+                }
+            }
+        }
+    }
+
+    playerGameboard.innerHTML = playGrid;
+    computerGameboard.innerHTML = playGrid;
 }
 
 document.addEventListener('DOMContentLoaded', gameBoard());
+
+let testDiv = document.getElementById('p52');
+let gridLocation = testDiv.className;
+testDiv.innerHTML += "<img src='./assets/images/ships/battleship.png' class='ship'>";
+testDiv.innerHTML += `<img src='./assets/images/effects/explosion1.gif' id='explode-${gridLocation}' class='explosion'>`;
+setTimeout(function () {
+    document.querySelector('[id^="explode-"]').remove();
+    testDiv.innerHTML += "<img src='./assets/images/effects/fire.gif' class='fire'>";
+}, 3700);
