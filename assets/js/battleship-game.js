476f28d6dde093d55e9fa039baa11d9d0b6487e6
mat-shepherd <>
2023-05-15 12:08:43 +0000

    Feat: create gameBoard function and variables to populate player and computer gameboard grids

---
+++ assets/js/battleship-game.js
@@ -0,0 +1,9 @@
+function gameBoard() {
+    let playerGameboard = document.getElementById('player-gameboard');
+    console.log(playerGameboard);
+    let computerGameboard = document.getElementById('computer-gameboard');
+    let gridLetters = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    let playGrid = '';
+}
+
+document.addEventListener('DOMContentLoaded', gameBoard());
